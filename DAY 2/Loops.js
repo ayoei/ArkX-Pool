c31ef79583ac416d04de6f4d08a62a788809f070
ayoei <elharaouiayoub19@gmail.com>
2024-01-10 10:08:58 +0100

    ark x challenge Day 2

--- DAY 2/Loops.js
+++ DAY 2/Loops.js
@@ -15,24 +15,56 @@ console.log(factorial);
 
 // Task 2 : How many digits ?
 
-var num2 = 123542;
+// var num2 = 123542;
 
-// Your program
-let count = 0;
+// // Your program
+// let count = 0;
 
-while (num2 >  0) {
-  num2 = Math.floor(num2/10);
-  count++;
+// while (num2 >  0) {
+//   num2 = Math.floor(num2/10);
+//   count++;
+// }
+// console.log(count);
+// ==========================
+
+let num2 = 123542;
+let counter = 0;
+
+if (num2 === 0) {
+  counter = 1;
+} else {
+  while (num2 >= 1 || num2 <= -1) {
+    num2 = num2 / 10;
+    counter++;
+  }
 }
-console.log(count);
+
+console.log(counter);
 
 // Output : 6
 
 
 // Task 3 :  Time to draw !
 let height = 4;
-for (i = 1; i <= height; i++) {
-    console.log((" ").repeat(height - i) + ("*").repeat(2 * i - 1));
+for (let i = 0; i <= height; i++) {
+  let space = "";
+  for (let j = 0; j < height - i; j++) {
+    space += " ";
+  }
+  let star = "*";
+  for (let k = 0; k < 2 * i; k++) {
+    star += "*";
+
+  }
+  let tree = space + star;
+  console.log(tree);
+
 }
-console.log((" ").repeat(height - 1) + "|");
+let trunkSpace = " ";
+let trunk = "|";
+for (let i = 0; i < height - 1; i++) {
+  trunkSpace += " ";
+}
+let trunkResult = trunkSpace + trunk;
+console.log(trunkResult);
 
